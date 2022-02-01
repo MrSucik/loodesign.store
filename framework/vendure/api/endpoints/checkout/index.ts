d866c8dc6159a63db8ece69730ba6a23966e9d42
Daniel Suchan <mr.sucik@gmail.com>
2022-02-01 18:48:25 +0800

    Remove new checkout endpoints in favor of modifying frameworks endpoint

--- framework/vendure/api/endpoints/checkout/index.ts
+++ framework/vendure/api/endpoints/checkout/index.ts
@@ -2,6 +2,22 @@ import { NextApiHandler } from 'next'
 import { CommerceAPI, createEndpoint, GetAPISchema } from '@commerce/api'
 import { CheckoutSchema } from '@commerce/types/checkout'
 import checkoutEndpoint from '@commerce/api/endpoints/checkout'
+import commerce from '@lib/api/commerce'
+import { getCommerceApi } from '@framework/api'
+import { Headers } from 'node-fetch'
+import createStripe from 'stripe'
+
+const activeOrderQuery = /* GraphQL */ `
+  query activeOrder {
+    activeOrder {
+      id
+      totalWithTax
+    }
+  }
+`
+const stripe = new createStripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2020-08-27',
+})
 
 const getCheckout: CheckoutEndpoint['handlers']['getCheckout'] = async ({
   req,
@@ -9,32 +25,37 @@ const getCheckout: CheckoutEndpoint['handlers']['getCheckout'] = async ({
   config,
 }) => {
   try {
-    const html = `
-      <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Checkout</title>
-        </head>
-        <body>
-          <div style='margin: 10rem auto; text-align: center; font-family: SansSerif, "Segoe UI", Helvetica; color: #888;'>
-             <svg xmlns="http://www.w3.org/2000/svg" style='height: 60px; width: 60px;' fill="none" viewBox="0 0 24 24" stroke="currentColor">
-               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-             </svg>
-             <h1>Checkout not yet implemented :(</h1>
-             <p>
-             See <a href='https://github.com/vercel/commerce/issues/64' target='_blank'>#64</a>
-             </p>
-          </div>
-        </body>
-      </html>
-    `
+    const requestCookie = req.headers.cookie as string
+    const response = await config.fetch(
+      activeOrderQuery,
+      {},
+      { headers: { cookie: requestCookie } }
+    )
 
-    res.status(200)
-    res.setHeader('Content-Type', 'text/html')
-    res.write(html)
-    res.end()
+    const product = await stripe.products.create({ name: 'Test product' })
+    // Generate price
+    const price = await stripe.prices.create({
+      product: product.id,
+      unit_amount: 2000,
+      currency: 'czk',
+    })
+
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+          price: price.id,
+          quantity: 1,
+        },
+      ],
+      payment_method_types: ['card'],
+      mode: 'payment',
+      success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.origin}/?canceled=true`,
+    })
+
+    res.send(response)
   } catch (error) {
     console.error(error)
 
