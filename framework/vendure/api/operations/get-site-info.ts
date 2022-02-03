c821d431c6b91602ceaeee3943a7b42adbe675ff
Daniel Suchan <mr.sucik@gmail.com>
2022-02-03 11:28:11 +0800

    Log categories

--- framework/vendure/api/operations/get-site-info.ts
+++ framework/vendure/api/operations/get-site-info.ts
@@ -39,6 +39,7 @@ export default function getSiteInfoOperation({
     }))
     const categories = arrayToTree(collections).children
     const brands = [] as any[]
+    console.log(categories, brands)
 
     return {
       categories: categories ?? [],
