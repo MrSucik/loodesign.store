d9abc8b7ee27e1b1e4e72b072e93be39674b56ca
Daniel Suchan <mr.sucik@gmail.com>
2022-02-03 10:36:47 +0800

    Move Statcounter to the document template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,15 @@ class MyDocument extends Document {
         <body className="loading">
           <Main />
           <NextScript />
+          <script type="text/javascript">
+            var sc_project=11426063; var sc_invisible=1; var
+            sc_security="9cf59078";
+          </script>
+          <script
+            type="text/javascript"
+            src="https://www.statcounter.com/counter/counter.js"
+            async
+          ></script>
         </body>
       </Html>
     )
