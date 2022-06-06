2ec7ba3d62ee5c64ac8e7311a2dcabb4fad2f5ea
Daniel Suchan <mr.sucik@gmail.com>
2022-06-06 21:39:49 +0200

    Export search page as default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,5 @@
-import commerce from '@lib/api/commerce'
-import { Layout } from '@components/common'
-import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
-import HomePage from '@components/common/HomePage'
+import Search, { getStaticProps } from './search'
 
-export async function getStaticProps({
-  preview,
-  locale,
-  locales,
-}: GetStaticPropsContext) {
-  const config = { locale, locales }
-  const productsPromise = commerce.getAllProducts({
-    variables: { first: 6 },
-    config,
-    preview,
-  })
-  const pagesPromise = commerce.getAllPages({ config, preview })
-  const siteInfoPromise = commerce.getSiteInfo({ config, preview })
-  const { products } = await productsPromise
-  const { pages } = await pagesPromise
-  const { categories, brands } = await siteInfoPromise
+export { getStaticProps }
 
-  return {
-    props: {
-      products,
-      categories,
-      brands,
-      pages,
-    },
-    revalidate: 60,
-  }
-}
-
-export default function Home({
-  products,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <></>
-}
-
-Home.Layout = Layout
+export default Search
