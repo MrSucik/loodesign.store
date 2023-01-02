ed1be3fa5af995472316a177bac114726c6a894b
Marco Velan <marco.velan10@gmail.com>
2023-01-02 01:21:16 +0100

    add blank pages

---
+++ pages/notice.tsx
@@ -0,0 +1,33 @@
+import type { GetStaticPropsContext } from 'next'
+import commerce from '@lib/api/commerce'
+import { Layout } from '@components/common'
+import { Text } from '@components/ui'
+
+export async function getStaticProps({
+  preview,
+  locale,
+  locales,
+}: GetStaticPropsContext) {
+  const config = { locale, locales }
+  const { pages } = await commerce.getAllPages({ config, preview })
+  const { categories, brands } = await commerce.getSiteInfo({ config, preview })
+  return {
+    props: {
+      pages,
+      categories,
+      brands,
+    },
+    revalidate: 200,
+  }
+}
+
+export default function Terms() {
+  return (
+    <div className="max-w-2xl mx-8 sm:mx-auto py-20 flex flex-col items-center justify-center fit">
+      <Text variant="heading">Notice</Text>
+      <Text className="">Notice</Text>
+    </div>
+  )
+}
+
+Terms.Layout = Layout
