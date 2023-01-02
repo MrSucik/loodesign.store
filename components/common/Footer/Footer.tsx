5539ab7b169772729b586e989640fa38d55ec7ce
Marco Velan <marco.velan10@gmail.com>
2023-01-02 01:21:07 +0100

    add footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import type { Page } from '@commerce/types/page'
 import getSlug from '@lib/get-slug'
-import { Github, Vercel } from '@components/icons'
 import { Logo, Container } from '@components/ui'
 import { I18nWidget } from '@components/common'
 import s from './Footer.module.css'
@@ -15,11 +14,73 @@ interface Props {
   pages?: Page[]
 }
 
+const links = [
+  {
+    name: 'Všechny produkty',
+    url: '/',
+  },
+  {
+    name: 'Ochrana osobních údajů',
+    url: '/privacy',
+  },
+  {
+    name: 'Obchodní podmínky',
+    url: '/terms',
+  },
+  {
+    name: 'Sdělení kupujícímu',
+    url: '/notice',
+  },
+]
+
 const Footer: FC<Props> = ({ className, pages }) => {
   const { sitePages } = usePages(pages)
   const rootClassName = cn(s.root, className)
 
-  return <footer className={rootClassName}></footer>
+  return (
+    <footer className={rootClassName}>
+      <Container>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accent-2 py-12 text-primary bg-primary transition-colors duration-150">
+          <div className="col-span-1 lg:col-span-2">
+            <Link href="/">
+              <a className="flex flex-initial items-center font-bold md:mr-24">
+                <span className="rounded-full border border-accent-6 mr-2">
+                  <Logo />
+                </span>
+                <span>LOOdesign</span>
+              </a>
+            </Link>
+          </div>
+          <div className="col-span-1 lg:col-span-8">
+            <div className="grid md:grid-rows-4 md:grid-cols-3 md:grid-flow-col">
+              {[...links, ...sitePages].map((page) => (
+                <span key={page.url} className="py-3 md:py-0 md:pb-4">
+                  <Link href={page.url!}>
+                    <a className="text-accent-9 hover:text-accent-6 transition ease-in-out duration-150">
+                      {page.name}
+                    </a>
+                  </Link>
+                </span>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-1 lg:col-span-2 flex items-start lg:justify-end text-primary">
+            <div className="flex space-x-6 items-center h-10">
+              <I18nWidget />
+            </div>
+          </div>
+        </div>
+        <div className="pt-6 pb-10 flex flex-col md:flex-row justify-between items-center space-y-4 text-accent-6 text-sm">
+          <div>
+            <span>&copy; Všechna práva vyhrazena.</span>
+          </div>
+          <div className="flex items-center text-primary text-sm">
+            <span className="text-primary">Vytvořeno: 🐱</span>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  )
 }
 
 function usePages(pages?: Page[]) {
