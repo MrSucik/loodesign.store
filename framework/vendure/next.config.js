3de7801c37c1245e47ae27bb6d4b29bee541619b
Marco Velan <marco.velan10@gmail.com>
2023-02-02 13:09:34 +0100

    fix: loading images

--- framework/vendure/next.config.js
+++ framework/vendure/next.config.js
@@ -8,6 +8,7 @@ module.exports = {
       'demo.vendure.io',
       'loodesign-vendure.herokuapp.com',
       'loodesign-mrsucik.vercel.app',
+      'loodesign-vendure.up.railway.app',
       'loodesignstore.up.railway.app',
     ],
   },
