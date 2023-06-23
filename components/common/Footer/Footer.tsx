67fadb59d6c33fac3df82a785044a4bf6fafdf80
Marco Velan <marco.velan10@gmail.com>
2023-06-23 12:00:36 +0200

    fix(i18n): remove unused widget

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
           </div>
           <div className="col-span-1 lg:col-span-2 flex items-start lg:justify-end text-primary">
             <div className="flex space-x-6 items-center h-10">
-              <I18nWidget />
+              {/* <I18nWidget /> */}
             </div>
           </div>
         </div>
